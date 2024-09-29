35e845db5fcfb3c22758b1bd5c63fc517526dead
Simon Haines <nimon@hotmail.co.uk>
2024-09-29 15:16:36 +0100

    remove unwanted test

--- src/test/java/com/corndel/supportbank/exercises/D2E2Test.java
+++ src/test/java/com/corndel/supportbank/exercises/D2E2Test.java
@@ -12,22 +12,4 @@ public class D2E2Test {
     int actualSum = adder.add("nums.txt");
     assertEquals(expectedSum, actualSum, "The sum of numbers in nums.txt should be " + expectedSum);
   }
-
-  @Test
-  public void testAdderWithNonExistentFile() {
-    // Redirect standard error to capture error messages
-    java.io.ByteArrayOutputStream errContent = new java.io.ByteArrayOutputStream();
-    System.setErr(new java.io.PrintStream(errContent));
-
-    Adder adder = new Adder();
-    int result = adder.add("nonexistent.txt");
-
-    // Reset standard error
-    System.setErr(System.err);
-
-    String errorOutput = errContent.toString().trim();
-
-    assertEquals(0, result, "Adder.add should return 0 when file does not exist");
-    assertTrue(errorOutput.contains("Something went wrong"), "Error message should be printed when file is missing");
-  }
 }
